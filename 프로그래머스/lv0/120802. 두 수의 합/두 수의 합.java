fc710cbe725fb09b7b3d19fc13980be24ac94e8f
wndlsrhd25 <105850880+wndlsrhd25@users.noreply.github.com>
2023-03-19 00:25:38 +0900

    [level 0] Title: 두 수의 합, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120802. 두 수의 합/두 수의 합.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        int num3 = num1 + num2;
+        int answer = 0;
+        
+            if(num3>=-100000 && num3<=100000) {
+                answer = num3;
+                
+             }
+         return answer;
+     }
+}
